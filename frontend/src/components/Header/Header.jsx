f9be422190fa3db8f3b9140a884c37af4f33daea
saurabhrkt <chaurasiasaurabh299@gmail.com>
2025-07-05 13:18:26 +0530

    add to cart and remove from cart functionality added

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Pizza Panda</h2>
+        <p>Order your favorite pizza online</p>
+        <button>view menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
